7ece764dcea6899a4aea953c12d408cfdf9046f5
SzigetiPeter <szigeti.peter5@gmail.com>
2015-08-20 08:01:53 +0300

    Added initial project
    Added sphere for planet and first person controller.

---
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+public float gravity = -10;
+    public Rigidbody myRigidbody;
+
+    public void Attract(Transform body) {
+        myRigidbody = GetComponent<Rigidbody> ();
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        myRigidbody.AddForce (gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation (bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50f * Time.deltaTime);
+    }
+}
